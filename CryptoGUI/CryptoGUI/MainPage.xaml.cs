1c477fa5eca6e6459328d23fbf88bce9a7d964d5
Ville Pulkkinen <K1532@student.jamk.fi>
2017-11-06 14:58:13 +0200

    Todella kehdo gui, testiä

--- CryptoGUI/CryptoGUI/MainPage.xaml.cs
+++ CryptoGUI/CryptoGUI/MainPage.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Security.Cryptography;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,9 +24,74 @@ namespace CryptoGUI
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private string selected;
+
+
         public MainPage()
         {
             this.InitializeComponent();
+            selected = "";
         }
+
+        private void button_aes_Click(object sender, RoutedEventArgs e)
+        {
+            selected_algo.Text = "AES";
+
+        }
+
+        private void button_3des_Click(object sender, RoutedEventArgs e)
+        {
+            selected_algo.Text = "3DES";
+        }
+
+        private void button_rc2_Click(object sender, RoutedEventArgs e)
+        {
+            selected_algo.Text = "RC2";
+        }
+
+        private void encrypt_click(object sender, RoutedEventArgs e)
+        {
+            string data = "moi";
+        
+                Encrypt(selected,data);
+        }
+        private void Encrypt(string algo,string data)
+        {
+            
+            byte[] EncryptStringToBytes_Aes(string plainText, byte[] keyArray, byte[] IV)
+            {
+                byte[] encrypted;
+
+                using (Aes aesAlg = Aes.Create())
+                {
+                    aesAlg.Key = keyArray;
+                    aesAlg.IV = IV;
+                    ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+                    using (MemoryStream msEncrypt = new MemoryStream())
+                    {
+                        using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                        {
+                            using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                            {
+                                swEncrypt.Write(plainText);
+                            }
+                            encrypted = msEncrypt.ToArray();
+                        }
+                    }
+                }
+                return encrypted;
+            }
+            using (Aes myAes = Aes.Create())
+            {
+   
+                byte[] encrypted = EncryptStringToBytes_Aes(data, myAes.Key, myAes.IV);
+                Debug.WriteLine("Key : " + myAes.Key.ToString());
+                Debug.WriteLine("Encrypted: " + encrypted.ToString());
+              
+
+            }
+        }
+
+
     }
 }
